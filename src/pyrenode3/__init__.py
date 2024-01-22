135ee08fb3f1e5d6c6060747f92083271ab9598e
Franciszek Zdobylak <fzdobylak@antmicro.com>
2024-01-22 10:15:09 +0100

    [#54111] Add support for Renode build using dotnet
    Signed-off-by: Franciszek Zdobylak <fzdobylak@antmicro.com>

--- src/pyrenode3/__init__.py
+++ src/pyrenode3/__init__.py
@@ -1,17 +1,38 @@
 import importlib
+import logging
 import os
 
 from pyrenode3.loader import RenodeLoader
 
 if "PYRENODE_SKIP_LOAD" not in os.environ:
-    if (pkg := os.environ.get("PYRENODE_ARCH_PKG")) is not None:
-        RenodeLoader.from_arch(pkg)
+    runtime = os.environ.get("PYRENODE_RUNTIME", "mono")
 
-    elif (pkg := os.environ.get("PYRENODE_BUILD_DIR")) is not None:
-        RenodeLoader.from_mono_build(pkg)
+    if runtime not in ["mono", "coreclr"]:
+        raise ImportError(f"Runtime '{runtime}' not supported")
+
+    if os.environ.get("PYRENODE_PKG") and os.environ.get("PYRENODE_BUILD_DIR"):
+        raise ImportError(
+            "Both PYRENODE_PKG and PYRENODE_BUILD_DIR are set. Please unset one of them."
+        )
+
+    if package := os.environ.get("PYRENODE_PKG"):
+        if runtime == "mono":
+            RenodeLoader.from_mono_arch_pkg(package)
+        elif runtime == "coreclr":
+            raise ImportError("Using dotnet package is not supported.")
+
+    elif build_dir := os.environ.get("PYRENODE_BUILD_DIR"):
+        if runtime == "mono":
+            RenodeLoader.from_mono_build(build_dir)
+        elif runtime == "coreclr":
+            RenodeLoader.from_net_build(build_dir)
 
     if not RenodeLoader().is_initialized:
-        msg = "Renode not found. Please set PYRENODE_ARCH_PKG to the location of Renode Arch package."
+        msg = (
+            "Renode not found. Please set PYRENODE_ARCH_PKG to the location of Renode Arch package."
+            "Set PYRENODE_PKG to the location of Renode package or PYRENODE_BUILD_DIR to the"
+            "location of Renode build directory."
+        )
         raise ImportError(msg)
 
     # this prevents circular imports
--- src/pyrenode3/loader.py
+++ src/pyrenode3/loader.py
@@ -1,12 +1,14 @@
 import glob
+import logging
 import os
 import pathlib
+import sys
 import tarfile
 import tempfile
 from contextlib import contextmanager
 from typing import Union
 
-import clr
+from pythonnet import load as pythonnet_load
 
 from pyrenode3.singleton import MetaSingleton
 
@@ -47,7 +49,7 @@ class RenodeLoader(metaclass=MetaSingleton):
         return self.__bin_dir
 
     @classmethod
-    def from_arch(cls, path: "Union[str, pathlib.Path]"):
+    def from_mono_arch_pkg(cls, path: "Union[str, pathlib.Path]"):
         """Load Renode from Arch package."""
         path = pathlib.Path(path)
         temp = tempfile.TemporaryDirectory()
@@ -57,16 +59,80 @@ class RenodeLoader(metaclass=MetaSingleton):
         renode_dir = pathlib.Path(temp.name) / "opt/renode"
 
         loader = cls()
-        loader.__setup(renode_dir / "bin", renode_dir, temp=temp)
+        loader.__setup(
+            renode_dir / "bin",
+            renode_dir,
+            runtime="mono",
+            temp=temp,
+            add_dlls=["Renode.exe"]
+        )
 
         return loader
+
+    @staticmethod
+    def discover_bin_dir(renode_dir, runtime):
+        if "PYRENODE_BUILD_OUTPUT" in os.environ:
+            build_out = os.environ["PYRENODE_BUILD_OUTPUT"]
+            renode_build_dir = renode_dir / build_out
+
+            if not renode_build_dir.exists():
+                logging.critical(f"{renode_build_dir} doesn't exist.")
+                sys.exit(1)
+        else:
+            default = "output/bin/Release"
+            if runtime == "coreclr":
+                default += "/net*"
+
+            dirs = glob.glob(str(renode_dir / default))
+
+            if len(dirs) != 1:
+                logging.critical(
+                    f"Can't determine Renode directory using the '{renode_dir / default}' pattern. "
+                    f"Please specify its path (relative to {renode_dir}) in the "
+                    "PYRENODE_BUILD_OUTPUT variable."
+                )
+                sys.exit(1)
+
+            renode_build_dir = pathlib.Path(dirs[0])
+
+        logging.info(f"Using {renode_build_dir} as a directory with Renode binaries.")
+        return renode_build_dir
 
     @classmethod
     def from_mono_build(cls, path: "Union[str, pathlib.Path]"):
         """Load Renode from Mono build."""
         renode_dir = pathlib.Path(path)
         loader = cls()
-        loader.__setup(renode_dir / "output/bin/Release", renode_dir)
+        loader.__setup(
+            cls.discover_bin_dir(renode_dir, "mono"),
+            renode_dir,
+            runtime="mono",
+            add_dlls=["Renode.exe"]
+        )
+
+        return loader
+
+    @classmethod
+    def from_net_build(cls, path: "Union[str, pathlib.Path]"):
+        renode_dir = pathlib.Path(path)
+        renode_bin_dir = cls.discover_bin_dir(renode_dir, "coreclr")
+
+        # HACK: move libMonoPosixHelper.so to path where it is searched for
+        src = renode_bin_dir / "runtimes/linux-x64/native/libMonoPosixHelper.so"
+        dst = renode_bin_dir / "runtimes/linux-x64/lib/netstandard2.0/libMonoPosixHelper.so"
+        if not dst.exists():
+            src_path = os.path.relpath(src, dst.parent)
+            logging.warning("libMonoPosixHelper.so is not in the expected location. Creating symlink.")
+            logging.warning(f"{src_path} -> {dst}")
+            os.symlink(src_path, dst)
+
+        loader = cls()
+        loader.__setup(
+            renode_bin_dir,
+            renode_dir,
+            runtime="coreclr",
+            add_dlls=["runtimes/linux-x64/lib/netstandard2.0/Mono.Posix.NETStandard.dll"],
+        )
 
         return loader
 
@@ -79,23 +145,50 @@ class RenodeLoader(metaclass=MetaSingleton):
         finally:
             os.chdir(last_cwd)
 
+    def __load_runtime(self):
+        params = {}
+        if self.__runtime ==  "coreclr":
+            # When using .NET Renode and the runtimeconfig.json file is present we should use that
+            # to specify exactly the runtime that is expected.
+            runtime_config = self.__bin_dir / "Renode.runtimeconfig.json"
+            if runtime_config.exists():
+                params["runtime_config"]  = runtime_config
+            else:
+                logging.warning(
+                    "Can't find the Renode.runtimeconfig.json file. "
+                    "Will use a default pythonnet runtime settings."
+                )
+
+        pythonnet_load(self.__runtime, **params)
+
     def __load_asm(self):
-        dlls = [
-            *glob.glob(str(self.binaries / "*.dll")),
-            "Renode.exe",
-        ]
+        # Import clr here, because it must be done after the proper runtime is selected.
+        # If the runtime isn't loaded, the clr module loads the default runtime (mono) automatically.
+        # It is an issue when we use non-default runtime, e.g. coreclr.
+        import clr
+
+        dlls = [*glob.glob(str(self.binaries / "*.dll"))]
+        dlls.extend(self.__extra.get("add_dlls", []))
 
         for dll in dlls:
             clr.AddReference(str(self.binaries / dll))
 
-    def __setup(self, bin_dir: "Union[str, pathlib.Path]", renode_dir: "Union[str, pathlib.Path]", **kwargs):
+    def __setup(self,
+        bin_dir: "Union[str, pathlib.Path]",
+        renode_dir: "Union[str, pathlib.Path]",
+        runtime: str,
+        **kwargs,
+    ):
         if self.__initialized:
             msg = "RenodeLoader is already initialized"
             raise InitializationError(msg)
 
         self.__bin_dir = pathlib.Path(bin_dir).absolute()
         self.__renode_dir = pathlib.Path(renode_dir).absolute()
+        self.__runtime = runtime
         self.__extra = kwargs
+
+        self.__load_runtime()
 
         self.__load_asm()
 
