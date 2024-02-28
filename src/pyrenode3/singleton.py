a5f7a76f7dd503163494610ff70e9bdfff43639b
Franciszek Zdobylak <fzdobylak@antmicro.com>
2024-02-28 15:54:28 +0100

    [#55786] singleton: Add warning when initialized from DummyThread
    Singleton classes shouldn't be initialized from DummyThread's.
    DummyThread's represent threads that are not started nor managed by
    Python runtime, hence it is hard to provide safety when accessing shared
    structures.
    
    Signed-off-by: Franciszek Zdobylak <fzdobylak@antmicro.com>

--- src/pyrenode3/singleton.py
+++ src/pyrenode3/singleton.py
@@ -1,8 +1,18 @@
+import logging
+import threading
+
 class MetaSingleton(type):
     __instances = {}  # noqa: RUF012
 
     def __call__(cls, *args, **kwargs):
         if cls not in cls.__instances:
+            if isinstance(threading.current_thread(), threading._DummyThread):
+                logging.warning(
+                    f"{cls.__name__}, which is a singleton class, is initialized from a thread "
+                    "not started by Python itself. It is not safe and should be avoided. If you "
+                    "encounter any errors please consider initializing this class beforehand in "
+                    "the main Python thread."
+                )
             cls.__instances[cls] = super().__call__(*args, **kwargs)
 
         return cls.__instances[cls]
